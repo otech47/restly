22b67a935883914d5845fc77ec15532240a04e8c
Oscar Lafarga <otech47@gmail.com>
2015-11-02 14:11:38 -0500

    syntax, enable individual routes

--- restly.js
+++ restly.js
@@ -25,134 +25,131 @@ app.use(express.static(__dirname+'/public'));
 
 // wrapper for passing middleware to express
 restly.use = function(mw) {
-  app.use(mw);
-}
+    app.use(mw);
+};
+
+restly.get = app.get;
+restly.post = app.post;
+restly.put = app.put;
+restly.delete = app.delete;
 
 // init
 restly.init = function(r, opts) {
 
-  // make sure all our defaults are set
-  var opts = defaultOpts(opts);
+    // make sure all our defaults are set
+    opts = defaultOpts(opts);
 
-  // get our routes
-  var routesCollection = routes.getRoutes(r);
+    // get our routes
+    var routesCollection = routes.getRoutes(r);
 
-  // for each route
-  for(var rc in routesCollection) {
-    
-    var apicall = routesCollection[rc];
-    
-    // add authentication object to apicall
-    if (apicall.authentication) {
-      
-      // get authentication methods
-      apicall.authentication = routes.getAuthentication(r, apicall.authentication);
+    // for each route
+    for(var rc in routesCollection) {
 
-      // combine api call params with authentication params
-      apicall = routes.combineWithAuthentication(apicall);
+        var apicall = routesCollection[rc];
 
-      routesCollection[rc] = apicall;
+        // add authentication object to apicall
+        if (apicall.authentication) {
+
+            // get authentication methods
+            apicall.authentication = routes.getAuthentication(r, apicall.authentication);
+
+            // combine api call params with authentication params
+            apicall = routes.combineWithAuthentication(apicall);
+
+            routesCollection[rc] = apicall;
+        }
+
+        // get the full and correct path for the library
+        apicall.library = process.cwd()+"/"+opts.lib+apicall.library;
+
+        // and for the authentication library, if required
+        if (apicall.authentication && apicall.authentication.library) {
+            apicall.authentication.library = process.cwd()+"/"+opts.lib+apicall.authentication.library;
+        }
+
+        // are we enabling caching?
+        if (!opts.caching || !apicall.caching) {
+            apicall.caching = false;
+        }
+
+        else {
+            apicall.caching = caching.parseOptsCache(opts.caching);
+
+            // add special _use_cache param to API call
+            apicall.parameters._use_cache = {
+                "required": false,
+                "type": "bool",
+                "description":"Do you want to read from the cache",
+                "default": true
+            };
+        }
+
+        // set up a express listener for each call
+        switch(apicall.method) {
+            case 'put':
+                (function(ac) {
+                  app.put(ac.endpoint, function (req,res) {
+                    routes.parseRequest(ac, req, res);
+                  });
+                })(apicall);
+                break;
+            case 'post':
+                (function(ac) {
+                  app.post(ac.endpoint, function (req,res) {
+                    routes.parseRequest(ac, req, res);
+                  });
+                })(apicall);
+                break;
+            case 'delete':
+                (function(ac) {
+                  app.delete(ac.endpoint, function (req,res) {
+                    routes.parseRequest(ac, req, res);
+                  });
+                })(apicall);
+                break;   
+            case 'get':
+                (function(ac) {
+                  app.get(ac.endpoint, function (req,res) {
+                    routes.parseRequest(ac, req, res);
+                  });
+                })(apicall);
+                break;    
+        }
     }
 
-    // get the full and correct path for the library
-    apicall.library = process.cwd()+"/"+opts.lib+apicall.library;
-
-    // and for the authentication library, if required
-    if (apicall.authentication && apicall.authentication.library) {
-      apicall.authentication.library = process.cwd()+"/"+opts.lib+apicall.authentication.library;
-    } 
-
-    // are we enabling caching?
-    if (!opts.caching || !apicall.caching) {
-      apicall.caching = false;
-    }
-
-    else {
-      apicall.caching = caching.parseOptsCache(opts.caching);
-
-      // add special _use_cache param to API call
-      apicall.parameters._use_cache = {
-        "required": false,
-        "type": "bool",
-        "description":"Do you want to read from the cache",
-        "default": true
-      }
-    }
-
-    // set up a express listener for each call
-    switch(apicall.method) {
-      case 'put': 
-      
-        (function(ac) {
-          app.put(ac.endpoint, function (req,res) {
-            routes.parseRequest(ac, req, res);           
-          });
-        })(apicall);
-        break;
-
-      case 'post':
-
-        (function(ac) {
-          app.post(ac.endpoint, function (req,res) {
-            routes.parseRequest(ac, req, res);           
-          });
-        })(apicall);
-        break;
-
-      case 'delete': 
-
-        (function(ac) {
-          app.delete(ac.endpoint, function (req,res) {
-            routes.parseRequest(ac, req, res);           
-          });
-        })(apicall);
-        break;   
-
-      case 'get': 
-        
-        (function(ac) {
-          app.get(ac.endpoint, function (req,res) {
-            routes.parseRequest(ac, req, res);           
-          });
-        })(apicall);
-        break;    
-    }
-
-  }
-
-  // documentation page
-  app.get(opts.docs_endpoint, function(req, res) {
+    // documentation page
+    app.get(opts.docs_endpoint, function(req, res) {
 
     var sections = [];
     for (var r in routesCollection) {
       sections.push(routesCollection[r].section);
     }
 
-    sections = _.uniq(sections)
+    sections = _.uniq(sections);
 
     // prepare the page data
-    var page = { 
-                  routes: routesCollection,
-                  config: opts,
-                  section: false,
-                  sections: sections,
-                  docs_endpoint: opts.docs_endpoint
-                };
-    
+    var page = {
+      routes: routesCollection,
+      config: opts,
+      section: false,
+      sections: sections,
+      docs_endpoint: opts.docs_endpoint
+    };
+
     // render the channel list page
     res.render(process.cwd()+"/node_modules/restly/views/index.jade", page);
-    
-  });
 
-  // documentation page by section
-  app.get(opts.docs_endpoint+"section/:section", function(req, res) {
-    
+    });
+
+    // documentation page by section
+    app.get(opts.docs_endpoint+"section/:section", function(req, res) {
+
     var sections = [];
     for (var r in routesCollection) {
       sections.push(routesCollection[r].section);
     }
 
-    sections = _.uniq(sections)
+    sections = _.uniq(sections);
 
     // prepare the page data
     var page = { 
@@ -162,14 +159,14 @@ restly.init = function(r, opts) {
                   sections: sections,
                   docs_endpoint: opts.docs_endpoint
                 };
-    
+
     // render the channel list page
     res.render(process.cwd()+"/node_modules/restly/views/index.jade", page);
-    
-  });
 
-  // listen on the specified port
-  if (opts.protocol == 'https') {
+    });
+
+    // listen on the specified port
+    if (opts.protocol == 'https') {
     // set up for secure server
     var fs = require('fs');
     var https = require('https');
@@ -187,38 +184,37 @@ restly.init = function(r, opts) {
     }
 
     var credentials = { key: privateKey, cert: certificate };
-    
-    var server = https.createServer(credentials, app);
-    
-    server.listen(opts.port);
-  } else {
-      var server = app.listen(opts.port);
-  }
 
-  // this function is called when you want the server to die gracefully
-  // i.e. wait for existing connections
-  var gracefulShutdown = function() {
+    var server = https.createServer(credentials, app);
+
+    server.listen(opts.port);
+    } else {
+      var server = app.listen(opts.port);
+    }
+
+    // this function is called when you want the server to die gracefully
+    // i.e. wait for existing connections
+    var gracefulShutdown = function() {
     console.log("Received kill signal, shutting down gracefully.");
     server.close(function() {
       console.log("Closed out remaining connections.");
       process.exit()
     });
-    
+
     // if after 
     setTimeout(function() {
         console.error("Could not close connections in time, forcefully shutting down");
         process.exit()
-   }, 3*1000);
-    
-  }
+    }, 3*1000);
 
-  // listen for TERM signal .e.g. kill <pid>
-  process.on ('SIGTERM', gracefulShutdown);
+    }
 
-  // listen for INT signal e.g. Ctrl-C
-  process.on ('SIGINT', gracefulShutdown);
+    // listen for TERM signal .e.g. kill <pid>
+    process.on ('SIGTERM', gracefulShutdown);
 
-}
+    // listen for INT signal e.g. Ctrl-C
+    process.on ('SIGINT', gracefulShutdown);
+};
 
 // default options
 var defaultOpts = function(opts) {
@@ -276,7 +272,6 @@ var defaultOpts = function(opts) {
 
   // return
   return defaults;
-
 }
 
 // export
